a30220e9b1dd85ed12a862c93206b92c3e5f48c9
Nick Topping <nick.topping@mppglobal.com>
2015-12-18 16:50:32 +0000

    Working End-to-end flow

--- DBInterface.js
+++ DBInterface.js
@@ -9,7 +9,7 @@ function DBInterface(){
 
 method.CreateSchema = function() {
 	db.serialize(function() {
-		db.run("CREATE TABLE IF NOT EXISTS Log (DateCreated DATETIME, Time INT, QueueTime INT, CardMachine INT, CoffeeMachine INT);");
+		db.run("CREATE TABLE IF NOT EXISTS Log (DateCreated TEXT, Time INT, QueueTime INT, CardMachine INT, CoffeeMachine INT);");
 
 		db.run("CREATE TABLE IF NOT EXISTS Summary (Day INT, Time INT, CurrentAverage INT DEFAULT(0), NumberOfLogs INT DEFAULT(0));");
 
@@ -23,7 +23,6 @@ method.CreateSchema = function() {
 				{
 					for (var i = 0; i <= 4; i++) //loop for each day as an int (dont care about weekend)
 					{	
-	  					console.log("Inserting for " + i);
 		  				method.AddSummaryTimes(i);
 		  			}
 				}
@@ -82,32 +81,183 @@ method.GetGraphData = function(callback)
 	var today = new Date();
 	var dd = dayOfWeekAsInteger(today.getDay());
 
-	db.each("SELECT CurrentAverage FROM Summary WHERE Day = ? ORDER BY Time ASC",dd,function(err, row){
-		if(err){
-	  		console.log(err);
-				//break out
-		}
-		else{
-	  		currentAverages.push(row.CurrentAverage);
-		}
+	db.serialize(function() 
+	{
+		db.each("SELECT CurrentAverage FROM Summary WHERE Day = ? ORDER BY Time ASC",dd,function(err, row){
+			if(err){
+		  		console.log(err);
+					//break out
+			}
+			else{
+				if(row.CurrentAverage == null)
+				{
+		  			currentAverages.push(0);
+				}
+				else
+				{			
+		  			currentAverages.push(row.CurrentAverage);
+				}
+			}
+		},function()
+		{
+			db.each("SELECT AVG(QueueTime) as QueueTime, Time FROM Log WHERE DateCreated = date('now') GROUP BY Time",function(err, row){
+				if(err){
+			  		console.log(err);
+						//break out
+				}
+				else{
+
+					var obj = {
+							    key1: row.QueueTime,
+							    key2: row.Time
+							};
+
+			  		todaysData.push(obj);
+				}
+			},function()
+			{
+
+				var horribleArray = new Array(33);
+				for (var i = horribleArray.length-1; i >= 0; -- i) horribleArray[i] = 0;
+
+				//
+				for (var i = todaysData.length - 1; i >= 0; i--) 
+				{
+					 switch(todaysData[i].key2){
+					 	case 800:
+					 		horribleArray[0] = todaysData[i].key1;
+					 		break;
+					 	case 815:
+					 		horribleArray[1] = todaysData[i].key1;
+					 		break;
+					 	case 830:
+					 		horribleArray[2] = todaysData[i].key1;
+					 		break;
+					 	case 845:
+					 		horribleArray[3] = todaysData[i].key1;
+					 		break;
+					 	case 900:
+					 		horribleArray[4] = todaysData[i].key1;
+					 		break;
+					 	case 915:
+					 		horribleArray[5] = todaysData[i].key1;
+					 		break;
+					 	case 930:
+					 		horribleArray[6] = todaysData[i].key1;
+					 		break;
+					 	case 945:
+					 		horribleArray[7] = todaysData[i].key1;
+					 		break;
+					 	case 1000:
+					 		horribleArray[8] = todaysData[i].key1;
+					 		break;
+					 	case 1015:
+					 		horribleArray[9] = todaysData[i].key1;
+					 		break;
+					 	case 1030:
+					 		horribleArray[10] = todaysData[i].key1;
+					 		break;
+					 	case 1045:
+					 		horribleArray[11] = todaysData[i].key1;
+					 		break;
+					 	case 1100:
+					 		horribleArray[12] = todaysData[i].key1;
+					 		break;
+					 	case 1115:
+					 		horribleArray[13] = todaysData[i].key1;
+					 		break;
+					 	case 1130:
+					 		horribleArray[14] = todaysData[i].key1;
+					 		break;
+					 	case 1145:
+					 		horribleArray[15] = todaysData[i].key1;
+					 		break;
+					 	case 1200:
+					 		horribleArray[16] = todaysData[i].key1;
+					 		break;
+					 	case 1215:
+					 		horribleArray[17] = todaysData[i].key1;
+					 		break;
+					 	case 1230:
+					 		horribleArray[18] = todaysData[i].key1;
+					 		break;
+					 	case 1245:
+					 		horribleArray[19] = todaysData[i].key1;
+					 		break;
+					 	case 1300:
+					 		horribleArray[20] = todaysData[i].key1;
+					 		break;
+					 	case 1315:
+					 		horribleArray[21] = todaysData[i].key1;
+					 		break;
+					 	case 1330:
+					 		horribleArray[22] = todaysData[i].key1;
+					 		break;
+					 	case 1345:
+					 		horribleArray[23] = todaysData[i].key1;
+					 		break;
+					 	case 1400:
+					 		horribleArray[24] = todaysData[i].key1;
+					 		break;
+					 	case 1415:
+					 		horribleArray[25] = todaysData[i].key1;
+					 		break;
+					 	case 1430:
+					 		horribleArray[26] = todaysData[i].key1;
+					 		break;
+					 	case 1445:
+					 		horribleArray[27] = todaysData[i].key1;
+					 		break;
+					 	case 1500:
+					 		horribleArray[28] = todaysData[i].key1;
+					 		break;
+					 	case 1515:
+					 		horribleArray[29] = todaysData[i].key1;
+					 		break;
+					 	case 1530:
+					 		horribleArray[30] = todaysData[i].key1;
+					 		break;
+					 	case 1545:
+					 		horribleArray[31] = todaysData[i].key1;
+					 		break;
+					 	case 1600:
+					 		horribleArray[32] = todaysData[i].key1;
+					 		break;
+					 }
+				};
+				callback(currentAverages, horribleArray);
+			});
+
+		});
+
+		
 	});
 
-	db.each("SELECT QueueTime FROM Log WHERE DateCreated = date('now') ORDER BY Time ASC",function(err, row){
-		if(err){
-	  		console.log(err);
-				//break out
-		}
-		else{
-	  		todaysData.push(row.CurrentAverage);
-		}
-	});
-
-	callback(currentAverages, todaysData);
+	
 }
 
 method.InsertLog = function(actualTime, queueTime, cardMachineWorking, coffeeMachineWorking) 
 {
-	db.run("INSERT INTO Log(Time, QueueTime, CardMachine, CoffeeMachine) VALUES(?,?,?,?)", actualTime, queueTime, cardMachineWorking, coffeeMachineWorking);
+	//Get 15 mins from queueTime
+	hour = actualTime.getHours();
+	mins = (Math.round(actualTime.getMinutes()/15) * 15) % 60;
+
+	if(hour < 8 || hour > 16 ){
+		return;
+	}
+
+	//putting the Hack in HackDay
+	if(mins == 0)
+	{
+		hour = hour+1;
+		var parsedTime = hour+""+mins+"0";
+	}
+	else{
+		var parsedTime = hour+""+mins;
+	}
+
+	var dateToSave = convertDate(actualTime);
+	db.run("INSERT INTO Log(DateCreated, Time, queueTime, CardMachine, CoffeeMachine) VALUES(?,?,?,?,?)", dateToSave, parsedTime, queueTime, cardMachineWorking, coffeeMachineWorking);
 	method.InsertSummary(actualTime, queueTime);
 }
 
@@ -118,7 +268,7 @@ method.InsertSummary = function(actualTime, queueTime)
 	mins = (Math.round(actualTime.getMinutes()/15) * 15) % 60;
 
 	//putting the Hack in HackDay
-	if(mins == 0)
+	if(mins == 0 && actualTime.getMinutes() > 50)
 	{
 		hour = hour+1;
 		var completeTime = hour+""+mins+"0";
@@ -140,6 +290,8 @@ method.InsertSummary = function(actualTime, queueTime)
 			//calculate average
 			newNumberOfRows = row.NumberOfLogs + 1;	
 
+			var newAverage = row.CurrentAverage + ((queueTime - row.CurrentAverage) / newNumberOfRows);
+
 			//Save updated date
 			db.run("UPDATE Summary SET CurrentAverage = ?, NumberOfLogs = ? WHERE rowid = ?", newAverage, newNumberOfRows, row.rowid);
 		}
@@ -149,6 +301,11 @@ method.InsertSummary = function(actualTime, queueTime)
 function dayOfWeekAsInteger(day) {
 	var dayArray = ["Sunday","0","1","2","3","4","Saturday"]
   	return dayArray[day];
+}
+
+function convertDate(date){
+	var month = date.getMonth() + 1; // BECAUSE WHYYYYYY
+	return date.getFullYear()+"-"+month+"-"+date.getDate()
 }
 
 module.exports = DBInterface;
